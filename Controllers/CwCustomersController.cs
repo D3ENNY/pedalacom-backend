818e7e8929d47e35582968a59ab3ae14953f0985
D3ENNY <denysraimondi06@gmail.com>
2024-01-10 17:49:10 +0100

    setup log error on cwCustomer  controller

--- Controllers/CwCustomersController.cs
+++ Controllers/CwCustomersController.cs
@@ -4,6 +4,7 @@ using WebAppPedalaCom.Models;
 using System.Security.Cryptography;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.Data.SqlClient;
+using WebAppPedalaCom.Blogic.Service;
 
 namespace WebAppPedalaCom.Controllers
 {
@@ -12,32 +13,58 @@ namespace WebAppPedalaCom.Controllers
     public class CwCustomersController : ControllerBase
     {
         private readonly CredentialWorks2024Context _CWcontext;
+        private readonly ErrorLogService _errorLogService;
 
-        public CwCustomersController(CredentialWorks2024Context _context) => this._CWcontext = _context;
+        public CwCustomersController(CredentialWorks2024Context _context)
+        {
+            this._CWcontext = _context;
+            CredentialWorks2024Context CWcontext = new();
+            this._errorLogService = new(CWcontext);
+        }
 
         // GET: api/CwCustomers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CwCustomer>>> GetCwCustomers()
         {
             if (_CWcontext.CwCustomers == null)
-                return NotFound();
+            {
+                _errorLogService.LogError(new ArgumentNullException());
+                return StatusCode(500, "Internal Server Error\n_context.CwCustomers is Null");
+            }
 
-            return await _CWcontext.CwCustomers.ToListAsync();
+            return Ok(await _CWcontext.CwCustomers.ToListAsync());
         }
 
         // GET: api/CwCustomers/5
         [HttpGet("{email}")]
         public async Task<ActionResult<CwCustomer>> GetCwCustomer(string email)
         {
-            if (_CWcontext.CwCustomers == null)
-                return NotFound();
+            CwCustomer? cwCustomer = null;
 
-            var cwCustomer = await _CWcontext.CwCustomers.FirstOrDefaultAsync(e => e.EmailAddress == email);
+            if (_CWcontext.CwCustomers == null)
+            {
+                _errorLogService.LogError(new ArgumentNullException());
+                return StatusCode(500, "Internal Server Error\n_context.CwCustomers is Null");
+            }
+
+            try
+            {
+                cwCustomer = await _CWcontext.CwCustomers.FirstOrDefaultAsync(e => e.EmailAddress == email);
+            }
+            catch (ArgumentNullException ex)
+            {
+                _errorLogService.LogError(ex);
+            }
+            catch(Exception ex) 
+            {
+                _errorLogService.LogError(ex);
+            }
+
 
             if (cwCustomer == null)
                 return NotFound();
 
-            return cwCustomer;
+            return Ok(cwCustomer);
         }
 
         // PUT: api/CwCustomers/5
@@ -47,24 +74,28 @@ namespace WebAppPedalaCom.Controllers
             if (email != cwCustomer.EmailAddress)
                 return BadRequest();
 
-            var existingCustomer = await _CWcontext.CwCustomers.FirstOrDefaultAsync(e => e.EmailAddress == email);
-
-            existingCustomer.UpdateCustomer(cwCustomer);
-
-            if (existingCustomer == null)
-                return NotFound();
-
-            _CWcontext.Entry(existingCustomer).State = EntityState.Modified;
-
             try
             {
+                CwCustomer? existingCustomer = await _CWcontext.CwCustomers.FirstOrDefaultAsync(e => e.EmailAddress == email);
+
+                existingCustomer?.UpdateCustomer(cwCustomer);
+
+                if (existingCustomer == null)
+                    return NotFound();
+
+                _CWcontext.Entry(existingCustomer).State = EntityState.Modified;
+
                 await _CWcontext.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
+            catch(ArgumentNullException ex)
+            {
+                _errorLogService.LogError(ex);
+            }
+            catch (DbUpdateConcurrencyException ex)
             {
                 if (!CwCustomerExists(email))
                     return NotFound();
-                else throw;
+                _errorLogService.LogError(ex);
             }
 
             return NoContent();
@@ -74,59 +105,74 @@ namespace WebAppPedalaCom.Controllers
         [HttpPost]
         public async Task<ActionResult<CwCustomer>> PostCwCustomer(CwCustomer cwCustomer)
         {
+            CwCustomer? userExistNewDB = null;
 
             if (_CWcontext.CwCustomers == null)
-                return Problem("Entity set 'CredentialWorks2024Context.CwCustomers'  is null.");
-
-            using AdventureWorksLt2019Context _AWcontext = new();
-            Customer userExistOldDB = _AWcontext.Customers.FromSqlRaw($"select * from [SalesLT].[Customer] where EmailAddress = @email", new SqlParameter("@email", cwCustomer.EmailAddress)).FirstOrDefault();
-
-            if (userExistOldDB != null)
             {
-                if (userExistOldDB.FkCustomerId != null)
-                    //Nel caso in cui è già registrato anche nel nuovo database
-                    return Problem("Esisti già pirla");
-                else
-                {
-                    //Nel caso in cui non fosse già registrato nel nuovo databasio
-                    UpdatePwdCustomers(cwCustomer);
-                    await _CWcontext.SaveChangesAsync();
-
-                    CwCustomer newUser = _CWcontext.CwCustomers.FromSqlRaw($"select * from [dbo].[CwCustomer] where EmailAddress = @email", new SqlParameter("@email", cwCustomer.EmailAddress)).FirstOrDefault();
-
-                    SqlParameter emailParameter = new("@email", cwCustomer.EmailAddress);
-
-                    SqlParameter idParameter = new("@id", newUser.CustomerId);
-
-                    _AWcontext.Database.ExecuteSqlRaw($"UPDATE [SalesLT].[Customer] SET [NameStyle] = ''  " +
-                       $",[Title] = null ,[FirstName] = ''  ,[MiddleName] = null ,[LastName] = '' ,[Suffix] = null ,[CompanyName] = null ,[SalesPerson] = null " +
-                       $",[EmailAddress] = null ,[Phone] = null  ,[PasswordHash] = '' ,[PasswordSalt] = '' ,[ModifiedDate] = GETDATE() " +
-                       $",[FK_Customer_id] = {idParameter.ParameterName} WHERE EmailAddress = {emailParameter.ParameterName}",
-                       emailParameter, idParameter);
-
-
-                    return CreatedAtAction("GetCwCustomer", new { email = cwCustomer.EmailAddress }, cwCustomer);
-                }
+                _errorLogService.LogError(new ArgumentNullException());
+                return StatusCode(500, "Internal Server Error\n_context.CwCustomers is Null");
             }
 
-            CwCustomer userExistNewDB = _CWcontext.CwCustomers.FromSqlRaw($"select * from [dbo].[CwCustomer] where EmailAddress = @email", new SqlParameter("@email", cwCustomer.EmailAddress)).FirstOrDefault();
+            using AdventureWorksLt2019Context _AWcontext = new();
+
+            try
+            {
+                Customer? userExistOldDB = _AWcontext.Customers.FromSqlRaw($"select * from [SalesLT].[Customer] where EmailAddress = @email", new SqlParameter("@email", cwCustomer.EmailAddress)).FirstOrDefault();
+
+                if (userExistOldDB != null)
+                {
+                    if (userExistOldDB.FkCustomerId != null)
+                        //Nel caso in cui è già registrato anche nel nuovo database
+                        return Conflict();
+                    else
+                    {
+                        //Nel caso in cui non fosse già registrato nel nuovo databasio
+                        UpdatePwdCustomers(cwCustomer);
+                        await _CWcontext.SaveChangesAsync();
+
+                        CwCustomer? newUser = _CWcontext.CwCustomers.FromSqlRaw($"select * from [dbo].[CwCustomer] where EmailAddress = @email", new SqlParameter("@email", cwCustomer.EmailAddress)).FirstOrDefault();
+                        SqlParameter emailParameter = new("@email", cwCustomer.EmailAddress);
+                        SqlParameter idParameter = new("@id", newUser?.CustomerId);
+                        _AWcontext.Database.ExecuteSqlRaw($"UPDATE [SalesLT].[Customer] SET [NameStyle] = ''  " +
+                           $",[Title] = null ,[FirstName] = ''  ,[MiddleName] = null ,[LastName] = '' ,[Suffix] = null ,[CompanyName] = null ,[SalesPerson] = null " +
+                           $",[EmailAddress] = null ,[Phone] = null  ,[PasswordHash] = '' ,[PasswordSalt] = '' ,[ModifiedDate] = GETDATE() " +
+                           $",[FK_Customer_id] = {idParameter.ParameterName} WHERE EmailAddress = {emailParameter.ParameterName}",
+                           emailParameter, idParameter
+                        );
+                        return CreatedAtAction("GetCwCustomer", new { email = cwCustomer.EmailAddress }, cwCustomer);
+                    }
+                }
+
+                userExistNewDB = _CWcontext.CwCustomers.FromSqlRaw($"select * from [dbo].[CwCustomer] where EmailAddress = @email", new SqlParameter("@email", cwCustomer.EmailAddress)).FirstOrDefault();
+            }
+            catch(ArgumentNullException ex)
+            {
+                _errorLogService.LogError(ex);
+            }catch(Exception ex)
+            {
+                _errorLogService.LogError(ex);
+            }
 
             if (userExistNewDB != null)
-                return Conflict("Esisti già pirla");
+                return Conflict();
             else
             {
-                cwCustomer.ModifiedDate = DateTime.Now;
-
-                UpdatePwdCustomers(cwCustomer);
-
-                await _CWcontext.SaveChangesAsync();
-
-                CwCustomer newUser = _CWcontext.CwCustomers.FromSqlRaw($"select * from [dbo].[CwCustomer] where EmailAddress = @email", new SqlParameter("@email", cwCustomer.EmailAddress)).FirstOrDefault();
-
-                SqlParameter idParameter = new("@id", newUser.CustomerId);
-
-                _AWcontext.Database.ExecuteSqlRaw($"INSERT INTO [SalesLT].[Customer] ([NameStyle], [FirstName], [LastName], [PasswordHash], [PasswordSalt], [ModifiedDate], [FK_Customer_id]) VALUES('', '', '', '', '', GETDATE(), {idParameter.ParameterName})", idParameter);
-
+                try
+                {
+                    cwCustomer.ModifiedDate = DateTime.Now;
+                    UpdatePwdCustomers(cwCustomer);
+                    await _CWcontext.SaveChangesAsync();
+                    CwCustomer? newUser = _CWcontext.CwCustomers.FromSqlRaw($"select * from [dbo].[CwCustomer] where EmailAddress = @email", new SqlParameter("@email", cwCustomer.EmailAddress)).FirstOrDefault();
+                    SqlParameter idParameter = new("@id", newUser?.CustomerId);
+                    _AWcontext.Database.ExecuteSqlRaw($"INSERT INTO [SalesLT].[Customer] ([NameStyle], [FirstName], [LastName], [PasswordHash], [PasswordSalt], [ModifiedDate], [FK_Customer_id]) VALUES('', '', '', '', '', GETDATE(), {idParameter.ParameterName})", idParameter);
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    _errorLogService.LogError(ex);
+                }catch(Exception ex)
+                {
+                    _errorLogService.LogError(ex);
+                }
                 return CreatedAtAction("GetCwCustomer", new { email = cwCustomer.EmailAddress }, cwCustomer);
             }
         }
@@ -135,17 +181,34 @@ namespace WebAppPedalaCom.Controllers
         [HttpDelete("{email}")]
         public async Task<IActionResult> DeleteCwCustomer(string email)
         {
+            CwCustomer? cwCustomer = null;
             if (_CWcontext.CwCustomers == null)
-                return NotFound();
-  
-            CwCustomer cwCustomer = await _CWcontext.CwCustomers.FirstOrDefaultAsync(e => e.EmailAddress == email);
+            {
+                _errorLogService.LogError(new ArgumentNullException());
+                return StatusCode(500, "Internal Server Error\n_context.CwCustomers is Null");
+            }
 
-            if (cwCustomer == null)
-                return NotFound();
+            try
+            {
+                cwCustomer = await _CWcontext.CwCustomers.FirstOrDefaultAsync(e => e.EmailAddress == email);
+                if (cwCustomer == null)
+                    return NotFound();
 
-            _CWcontext.CwCustomers.Remove(cwCustomer);
-            await _CWcontext.SaveChangesAsync();
-
+                _CWcontext.CwCustomers.Remove(cwCustomer);
+                await _CWcontext.SaveChangesAsync();
+            }
+            catch(ArgumentNullException ex)
+            {
+                _errorLogService.LogError(ex);
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                _errorLogService.LogError(ex);
+            }
+            catch (Exception ex)
+            {
+                _errorLogService.LogError(ex);
+            }
             return NoContent();
         }
 
@@ -154,7 +217,7 @@ namespace WebAppPedalaCom.Controllers
         private KeyValuePair<string, string> EncryptSaltString(string pwdNeedToHash)
         {
             byte[] byteSalt = new byte[16];
-            string EncSalt, EncResult;
+            string EncSalt = string.Empty, EncResult = string.Empty;
  
             try
             {
@@ -170,9 +233,9 @@ namespace WebAppPedalaCom.Controllers
                 );
                 EncSalt = Convert.ToBase64String(byteSalt);
             }
-            catch (Exception)
+            catch (ArgumentNullException ex)
             {
-                throw;
+                _errorLogService.LogError(ex);
             }
 
             return new KeyValuePair<string, string>(EncSalt, EncResult);
@@ -180,7 +243,7 @@ namespace WebAppPedalaCom.Controllers
 
         private void UpdatePwdCustomers(CwCustomer customer)
         {
-            KeyValuePair<string, string> hashpass = EncryptSaltString(customer.PasswordHash);
+            KeyValuePair<string, string> hashpass = EncryptSaltString(customer.PasswordHash ?? "");
 
             customer.PasswordHash = hashpass.Value;
 
