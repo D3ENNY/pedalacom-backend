d980f2257154ad8dbcd89cb48cccd89389d8a1a1
D3ENNY <denysraimondi06@gmail.com>
2024-01-12 19:12:55 +0100

    add product description to post endpoint

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Newtonsoft.Json;
+using System.Text.Json;
 using WebAppPedalaCom.Blogic.Service;
 using WebAppPedalaCom.Models;
 
@@ -239,64 +242,6 @@ namespace WebAppPedalaCom.Controllers
          *      */
 
         // PUT: api/Products/{id}
-
-
-        /*
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduct(int id, Product product)
-        {
-
-            string[] arr = product.ThumbnailPhotoFileName.Split(",");
-            
-            if(arr.Length == 2)
-            {
-                product.ThumbnailPhotoFileName = arr[1];
-            }
-            else
-            {
-                product.ThumbnailPhotoFileName = arr[0];
-            }
-            
-
-            Product newProduct = new()
-            {
-                ProductId = product.ProductId,
-                Color = product.Color,
-                ListPrice = product.ListPrice,
-                ModifiedDate = DateTime.Now,
-                Name = product.Name,
-                ProductCategoryId = product.ProductCategoryId,
-                ProductNumber = product.ProductNumber,
-                Size = product.Size,
-                StandardCost = product.StandardCost,
-                ThumbNailPhoto = Convert.FromBase64String(product.ThumbnailPhotoFileName),
-                Weight = product.Weight,
-                SellStartDate = DateTime.Now,
-            };
-
-           
-            _context.Entry(newProduct).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                if (!ProductExists(id))
-                    return NotFound();
-
-                _errorLogService.LogError(ex);
-            }
-            catch (Exception ex)
-            {
-                _errorLogService.LogError(ex);
-            }
-
-            return NoContent();
-        }
-            */
-
         [HttpPut("{id}")]
         public async Task<IActionResult> PutProduct(int id, Product product)
         {
@@ -349,24 +294,10 @@ namespace WebAppPedalaCom.Controllers
         
         // POST: api/Products
         [HttpPost]
-        public async Task<ActionResult<Product>> PostProduct(Product product)
+        public async Task<ActionResult<Product>> PostProduct(List<object>data)
         {
-            Product newProduct = new()
-            {
-                ProductId = product.ProductId,
-                Color = product.Color,
-                ListPrice = product.ListPrice,
-                ModifiedDate = DateTime.Now, 
-                Name = product.Name,
-                ProductCategoryId = product.ProductCategoryId,
-                ProductNumber = product.ProductNumber,
-                Size = product.Size,
-                StandardCost = product.StandardCost,
-                ThumbNailPhoto = Convert.FromBase64String(product.ThumbnailPhotoFileName.Split(",")[1]),
-                Weight = product.Weight,
-                SellStartDate = DateTime.Now,
-                Rowguid = Guid.NewGuid()
-            };
+            string? model = null, description = null;
+            Product product = null;
 
             if (_context.Products == null)
             {
@@ -374,18 +305,76 @@ namespace WebAppPedalaCom.Controllers
                 return StatusCode(500, "Internal Server Error\n_context.Product is Null");
             }
 
-            _context.Products.Add(newProduct);
+            if (data == null || data.Count < 2)
+                return StatusCode(500, "Internal Server Error\n dara is Null");
 
-            try
+            if (data[0] is JsonElement jsonElement && data[1] is JsonElement array)
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException ex)
-            {
-                _errorLogService.LogError(ex);
-            }
+                JsonElement first = array.EnumerateArray().FirstOrDefault();
+                JsonElement last = array.EnumerateArray().LastOrDefault();
 
-            return CreatedAtAction("GetProducts", new { id = newProduct.ProductId }, newProduct);
+                if (!first.Equals(default(JsonElement)) && !last.Equals(default(JsonElement)))
+                {
+                    model = first.GetString();
+                    description = last.GetString();
+                }
+                product = new()
+                {
+                    ProductId = jsonElement.GetProperty("productId").GetInt16(),
+                    Color = jsonElement.GetProperty("color").GetString(),
+                    ListPrice = jsonElement.GetProperty("listPrice").GetInt16(),
+                    ModifiedDate = DateTime.Now,
+                    Name = jsonElement.GetProperty("name").GetString() ?? string.Empty,
+                    ProductCategoryId = jsonElement.GetProperty("productCategoryId").GetInt16(),
+                    ProductNumber = jsonElement.GetProperty("productNumber").GetString() ?? string.Empty,
+                    Size = jsonElement.GetProperty("size").GetString(),
+                    StandardCost = jsonElement.GetProperty("standardCost").GetInt16(),
+                    ThumbNailPhoto = Convert.FromBase64String(jsonElement.GetProperty("thumbnailPhotoFileName").GetString().Split(",")[1]),
+                    Weight = jsonElement.GetProperty("weight").GetInt16(),
+                    SellStartDate = DateTime.Now,
+                    Rowguid = Guid.NewGuid()
+                };
+            }
+            using (IDbContextTransaction transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    ProductModel existingModel = await _context.ProductModels.FirstOrDefaultAsync(x => x.Name == model);
+                    if (existingModel == null)
+                    {
+                        _context.ProductModels.Add(new ProductModel() { Name = model, Rowguid = Guid.NewGuid(), ModifiedDate = DateTime.Now });
+                        await _context.SaveChangesAsync();
+                    }
+                    ProductDescription existingDescription = await _context.ProductDescriptions.FirstOrDefaultAsync(x => x.Description == description);
+                    if (existingDescription == null)
+                    {
+                        _context.ProductDescriptions.Add(new ProductDescription() { Description = description, Rowguid = Guid.NewGuid(), ModifiedDate = DateTime.Now });
+                        await _context.SaveChangesAsync();
+                    }
+                    int productModelId = (existingModel != null) ? existingModel.ProductModelId : _context.ProductModels.Single(x => x.Name == model).ProductModelId;
+                    int productDescId = (existingDescription != null) ? existingDescription.ProductDescriptionId : _context.ProductDescriptions.Single(x => x.Description == description).ProductDescriptionId;
+                    _context.ProductModelProductDescriptions.Add(new ProductModelProductDescription()
+                    {
+                        ProductModelId = productModelId,
+                        ProductDescriptionId = productDescId,
+                        Culture = "????",
+                        Rowguid = Guid.NewGuid(),
+                        ModifiedDate = DateTime.Now
+                    });
+                    product.ProductModelId = productModelId;
+                    _context.Products.Add(product);
+                    await _context.SaveChangesAsync();
+                    transaction.Commit();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    _errorLogService.LogError(ex);
+                    await Console.Error.WriteLineAsync("error -> " + ex.Message);
+                }
+            }
+            
+
+            return CreatedAtAction("GetProducts", new { id = product.ProductId }, product);
         }
         
 
